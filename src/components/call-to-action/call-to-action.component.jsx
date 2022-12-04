471f3e2ddc07e7a540071f7be1c77bd24bcd1c41
Geronimo Viscontti <visconttig@gmail.com>
2022-12-04 13:56:41 -0300

    Apply media queries styles to cta section and skills section.

--- src/components/call-to-action/call-to-action.component.jsx
+++ src/components/call-to-action/call-to-action.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./call-to-action.styles.scss";
-// import contactImage from "../../assets/images/contact-image.png";
 
 const CallToAction = () => {
 
@@ -10,15 +9,15 @@ const CallToAction = () => {
             <div className="container cta-card">
                 <h2 className="cta-title">Get in touch!</h2>
                 <div className="cta-container">
-                    <div className="cta-text-container">
-                    <div className="cta-text">
-                    <p>I'm hard-working, coachable and if I don't know something I promiss you what I can learn it (And I'll do it). </p>
-                    </div>
-                    <a href="mailto:visconttig@gmail.com?subject=Full-Stack%20[ACTION REQUIRED]&body=Hi%20Geronimo!%20I%20found%20your%20profile%20interesting%20and..." className="cta-button">Contact me</a>
-                    </div>
-                    <div className="cta-image-container">
-                        <img src={process.env.PUBLIC_URL + "/images/contact-image.png"} alt="" />
-                    </div>
+                        <div className="cta-text-container">
+                            <div className="cta-text">
+                            <p>I'm hard-working, coachable and if I don't know something I promiss you what I can learn it (And I'll do it). </p>
+                            </div>
+                            <a href="mailto:visconttig@gmail.com?subject=Full-Stack%20[ACTION REQUIRED]&body=Hi%20Geronimo!%20I%20found%20your%20profile%20interesting%20and..." className="cta-button">Contact me</a>
+                        </div>
+                        <div className="cta-image-container">
+                            <img src={process.env.PUBLIC_URL + "/images/contact-image.png"} alt="contact, phone and email icons" />
+                        </div>
                 </div>
             </div>
         </div>
