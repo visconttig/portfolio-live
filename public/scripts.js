4ab6553b8f41cc72712abe79c392c7974957ed32
Geronimo Viscontti <visconttig@gmail.com>
2022-12-04 14:18:59 -0300

    Add alert message.

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,8 @@
 document.onreadystatechange = () => {
     if(document.readyState === "complete"){
 
+        alert("Some of the links are down because the cloud services provider's subscription plans changed. \nI'm already aware and I'm working on it as I have time. \n\nThanks for understanding.");
+
     const mobileButton = document.querySelector("#mobile-button");
     const navigationSection = document.querySelector(".navigation-section");
     const navLinks = document.querySelectorAll("a.nav-link");
