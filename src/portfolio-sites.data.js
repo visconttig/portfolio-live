892155368b21b5cab2e97a6e369a1ae135a8ba2b
Geronimo Viscontti <visconttig@gmail.com>
2023-01-05 21:06:40 -0300

    Enable Spotify-clone site data.

--- src/portfolio-sites.data.js
+++ src/portfolio-sites.data.js
@@ -6,13 +6,13 @@ const SITES_DATA = [
     skills: ["React", "APIs", "CSS-Flexbox", "Tailwind CSS", "Axios"],
     siteUrl: "https://candid-banoffee-cf5b5e.netlify.app/"
 },
-// {
-//     title: "Spotify Clone",
-//     image: "/images/spotify-clone.jpg",
-//     description: "A spotify-like web app, with custom search, geolocation and more. **Not available at the moment** (Api calls quota exceeded in testing phase).",
-//     skills: ["React", "APIs", "Redux", "Axios"],
-//     siteUrl: "https://lively-pothos-f99b3f.netlify.app/"
-// },
+{
+    title: "Spotify Clone",
+    image: "/images/spotify-clone.jpg",
+    description: "A spotify-like web app, with custom search, geolocation and more.",
+    skills: ["React", "APIs", "Redux", "Axios"],
+    siteUrl: "https://lively-pothos-f99b3f.netlify.app/"
+},
 {
     title: "Crown Clothing",
     image: "/images/crwn-clothing.png",
