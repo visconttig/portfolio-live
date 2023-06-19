cc33c9cff5242d0a15ee5f6e9683d992d814e867
Geronimo Viscontti <visconttig@gmail.com>
2023-06-19 12:15:17 -0400

    Add Alt attribute.

--- src/components/live-site/live-site.component.jsx
+++ src/components/live-site/live-site.component.jsx
@@ -7,33 +7,36 @@ const LiveSite = ({ siteData }) => {
   return (
     <>
       <div className="main-container">
-      <h2 className="title title2">{title}</h2>
-      <div className="live-site-container">
-        <div className="image-container">
-          {/* <img src={image} /> */}
-          <img src={process.env.PUBLIC_URL + `${image}`} />
-          <button className="see-live-button">
-          <a target="_blank" rel="noopener noreferrer" href={siteUrl}>
-            SEE LIVE
-          </a>
-        </button>
-        </div>
-        <div className="description-container">
-          <p className="description"> {description} </p>
-          <div className="skills-row">
-            {skills.map((skill) => (
-              <ul>
-                <li className="skill"> {skill} </li>
-              </ul>
-            ))}
+        <h2 className="title title2">{title}</h2>
+        <div className="live-site-container">
+          <div className="image-container">
+            {/* <img src={image} /> */}
+            <img
+              src={process.env.PUBLIC_URL + `${image}`}
+              alt="Portfolio website"
+            />
+            <button className="see-live-button">
+              <a target="_blank" rel="noopener noreferrer" href={siteUrl}>
+                SEE LIVE
+              </a>
+            </button>
           </div>
-        </div>
-        {/* <button className="see-live-button">
+          <div className="description-container">
+            <p className="description"> {description} </p>
+            <div className="skills-row">
+              {skills.map((skill) => (
+                <ul>
+                  <li className="skill"> {skill} </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+          {/* <button className="see-live-button">
           <a target="_blank" rel="noopener noreferrer" href={siteUrl}>
             SEE LIVE
           </a>
         </button> */}
-      </div>
+        </div>
       </div>
     </>
   );
